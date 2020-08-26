14a036a05b9cc186b4abe14ac80cdebd8fd28828
Paul M2B <polo2bodi@gmail.com>
2020-08-26 18:40:37 +0200

    Add validation to submit contact request

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -1,13 +1,7 @@
 class QuestionMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.question_mailer.new_question.subject
-  #
   def new_question(question)
     @question = question
-    mail(from: "paul@mariage-participatif.fr", to: "polodile.mdb@gmail.com",  subject: "mariage-participatif.fr : Nouveau message de #{@question.name}")
+    mail(from: "paul@mariage-participatif.fr", to: "polo2bodi@gmail.com",  subject: "mariage-participatif.fr : Nouveau message de #{@question.name}")
   end
 end
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,54 @@
 class Question < ApplicationRecord
 
-validates :name, presence: true
-validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "un vrai mail pour une vraie réponse, svp" }
-validates :message, presence: true, length: { minimum: 33, too_short: "33 charactères minimum" }
-
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "un vrai mail pour une vraie réponse, svp" }
+  validates :message, presence: true, length: { minimum: 33, too_short: "33 charactères minimum" }
+  validate :sum_is_correct
   after_create :send_question
 
+  def text_number_1
+    number_as_text(number_1)
+  end
+
+  def text_number_2
+    number_as_text(number_2)
+  end
+
   private
+
+  def number_as_text(number)
+    case number
+    when 0
+      "zero"
+    when 1
+      "un"
+    when 2
+      "deux"
+    when 3
+      "trois"
+    when 4
+      "quatre"
+    when 5
+      "cinq"
+    when 6
+      "six"
+    when 7
+      "sept"
+    when 8
+      "huit"
+    when 9
+      "neuf"
+    when 10
+      "dix"
+    end
+  end
+
+  def sum_is_correct
+    return unless number_1 && number_2
+    unless number_1 + number_2 == security_answer
+      errors[:base] << "Alert SPAM"
+    end
+  end
 
   def send_question
     QuestionMailer.new_question(self).deliver_now
